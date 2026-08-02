c994fb640757ced915d757ee175c3e87ca74a497
Samiul821 <smsamiul821@gmail.com>
2026-08-02 22:31:20 +0600

    routing and main layout setup done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <div>
-      <h1 className="text-2xl">Main Layout</h1>
+      <main>
+        <Outlet></Outlet>
+      </main>
     </div>
   );
 };
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home/Home";
 
-const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
 
-export default Router;
+export default router;
